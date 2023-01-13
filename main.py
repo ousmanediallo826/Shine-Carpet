81ac6d459c7469d4d2a0edfd4c36150f4746c91d
Ousmane Diallo <106568955+ousmanediallo826@users.noreply.github.com>
2023-01-13 01:04:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+X_REPEAT = 3  # How many times to tessellate horizontally.
+Y_REPEAT = 4  # How many times to tessellate vertically.
+  
+for i in range(Y_REPEAT):
+    print(r'___|___|___|___|' * X_REPEAT)
+    print(r'___|___|___|___|' * X_REPEAT)
+    print(r'___|___|___|___|' * X_REPEAT)
+    print(r'___|___|___|___|' * X_REPEAT)
+    print(r'___|___|___|___|' * X_REPEAT)
+    print(r'___|___|___|___|' * X_REPEAT)
